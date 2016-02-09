56243650bd1605e06ddfcb4a6fe8febe477c2324
Erik Schnetter <schnetter@gmail.com>
2016-02-09 11:15:55 -0500

    Initial import

--- src/TaskScheduling.jl
+++ src/TaskScheduling.jl
@@ -1,5 +1,58 @@
 module TaskScheduling
 
-# package code goes here
+using Base.Threads
 
-end # module
+type Scheduler
+    tasks::Vector{Any}
+    mutex::Mutex
+    nbusy::Atomic{Int}
+    terminate::Atomic{Int}
+    Scheduler() = new(Any[], Mutex(), Atomic{Int}(), Atomic{Int}())
+end
+const scheduler = Scheduler()
+
+export submit
+function submit(task)
+    lock!(scheduler.mutex)
+    push!(scheduler.tasks, task)
+    unlock!(scheduler.mutex)
+end
+
+function gettask()
+    # get lock
+    lock!(scheduler.mutex)
+    # get next task, if any
+    task = nothing
+    if !isempty(scheduler.tasks)
+        # get task
+        task = pop!(scheduler.tasks)
+        atomic_add!(scheduler.nbusy, 1)
+    else
+        # check for termination
+        if scheduler.nbusy[] == 0
+            scheduler.terminate[] = 1
+        end
+    end
+    # release lock
+    unlock!(scheduler.mutex)
+    task
+end
+
+export runtasks
+function runtasks()
+    scheduler.nbusy[] = 0
+    scheduler.terminate[] = 0
+    @threads for i in 1:nthreads()
+        while scheduler.terminate[] == 0
+            # get next task
+            task = gettask()
+            # run the task
+            if task !== nothing
+                task()
+                atomic_sub!(scheduler.nbusy, 1)
+            end
+        end
+    end
+end
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,60 @@
+using Base.Threads
 using TaskScheduling
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+# Simple test
+flag = false
+submit() do
+    global flag = true
+end
+runtasks()
+@test flag == true
+
+# Run many trivial tasks
+counter = Atomic{Int}(0)
+for i in 1:100
+    submit() do
+        global counter
+        atomic_add!(counter, 1)
+    end
+end
+runtasks()
+@test counter[] == 100
+
+# Run a long sequence of tasks
+counter = Atomic{Int}(0)
+function run1(i::Integer)
+    global counter
+    i==0 && return
+    atomic_add!(counter, 1)
+    submit() do
+        run1(i-1)
+    end
+end
+submit() do
+    run1(100)
+end
+runtasks()
+@test counter[] == 100
+
+# Run a tree of tasks
+counter = Atomic{Int}(0)
+function run2(i::Integer)
+    global counter
+    i==0 && return
+    atomic_add!(counter, 1)
+    j = i-1
+    j1 = div(j,2)
+    j2 = j - j1
+    submit() do
+        run2(j1)
+    end
+    submit() do
+        run2(j2)
+    end
+end
+submit() do
+    run2(100)
+end
+runtasks()
+@test counter[] == 100
